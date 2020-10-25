5219aa5db2757feb1777fba9102f90177c664fe8
Anastasios-Faidon Retselis <anast.rets@hotmail.com>
2020-10-25 19:43:20 +0000

    Add Chapter2_1

---
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_1.m
@@ -0,0 +1,16 @@
+clear;
+clc;
+
+while 1==1
+    disp('Coin Flip Simulator');
+    number_toss = input('Enter number of tosses to be performed: ')  
+    
+    results = randi([0:1], [number_toss,1]);
+    heads = sum(results(:,1)==0);
+    tails = sum(results(:,1)==1);
+    sum = heads + tails;
+    fprintf('Number of heads = %d\n', heads);
+    fprintf('Number of tails = %d\n', tails);
+    fprintf('Total = %d\n', sum);    
+    clear;
+end
