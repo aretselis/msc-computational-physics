0a16ef50824685546bfa51861bff5e835f141f31
retse <anast.rets@hotmail.com>
2020-10-26 15:21:51 +0200

    Include flips based on continuous distribution and plot data

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_1.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_1.m
@@ -1,16 +1,45 @@
 clear;
 clc;
 
-disp('Coin Flip Simulator');
-n = input('Enter number of tosses to be performed: ')  
-    
-results = zeros(n,4); % Preallocate an array 
-for i = 1:n
-    toss = randi([0:1], [i,1]);
-    results(i,1) = i;
-    results(i,2) = sum(toss(:,1)==0); % Count heads ( ==0)
-    results(i,3) = sum(toss(:,1)==1); % Count tails ( ==1)
-    results(i,4) = results(i,3)/i;
-end
+disp('Coin Flipping Simulator');
+n = input('Enter number of tosses to be performed: ');  
+results = zeros(n,4); % Preallocate results array 
 
-scatter(results(:,1),results(:,4))
+%Discrete Method (Heads=0, Tails=1)
+for i = 1:n
+    flips = randi([0:1], [i,1]);
+    results(i,1) = i; % Log current number of flips 
+    results(i,2) = sum(flips(:,1)==0); % Count heads ( ==0)
+    results(i,3) = sum(flips(:,1)==1); % Count tails ( ==1)
+    results(i,4) = results(i,3)/i; % Count tails percentage
+end
+% Plot results and reset array for next run
+figure
+scatter(results(:,1),results(:,4),'.')
+title({'Coin flips from discrete method (0=Heads, 1=Tails)',''})
+xlabel('Number of flips, n')
+ylabel('Number of Heads/Total Flips')
+results = zeros(n,4); % Reset results array
+
+% Continuous Distribution in [0,1] (Heads<0.5, Tails>0.5)
+for i = 1:n
+    flips = rand(i,1);
+    for j = 1:i
+        if flips(j,1)< 0.5 % If <0.5 is is heads
+            flips(j,1) = 0;
+        else % If >0.5 it is tails
+            flips(j,1) = 1;
+        end
+    end
+    results(i,1) = i; % Log current number of flips 
+    results(i,2) = sum(flips(:,1)==0); % Count heads ( ==0)
+    results(i,3) = sum(flips(:,1)==1); % Count tails ( ==1)
+    results(i,4) = results(i,3)/i; % Count tails percentage
+end
+% Plot results
+figure
+scatter(results(:,1),results(:,4),'.','r')
+title({'Coin flips from continuous distribution in [0,1]','(Heads if <0.5, Tails if >0.5)',''})
+xlabel('Number of flips, n')
+ylabel('Number of Heads/Total Flips')
+
