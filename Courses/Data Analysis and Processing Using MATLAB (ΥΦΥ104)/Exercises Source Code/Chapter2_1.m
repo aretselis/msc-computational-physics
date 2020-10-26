8e570e394dc8674f4dedd7ef4bf9eb5beb66081b
retse <anast.rets@hotmail.com>
2020-10-26 15:28:48 +0200

    Include grid lines to plot outputs

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_1.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_1.m
@@ -19,6 +19,7 @@ scatter(results(:,1),results(:,4),'.')
 title({'Coin flips from discrete method (0=Heads, 1=Tails)',''})
 xlabel('Number of flips, n')
 ylabel('Number of Heads/Total Flips')
+grid on
 results = zeros(n,4); % Reset results array
 
 % Continuous Distribution in [0,1] (Heads<0.5, Tails>0.5)
@@ -42,4 +43,5 @@ scatter(results(:,1),results(:,4),'.','r')
 title({'Coin flips from continuous distribution in [0,1]','(Heads if <0.5, Tails if >0.5)',''})
 xlabel('Number of flips, n')
 ylabel('Number of Heads/Total Flips')
+grid on
 
