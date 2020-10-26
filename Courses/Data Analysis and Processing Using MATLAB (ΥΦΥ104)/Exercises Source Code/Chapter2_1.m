5112185c0ae2e04472b8f743249641ab506e30f7
retse <anast.rets@hotmail.com>
2020-10-26 14:17:18 +0200

    Include coin toss simulation with discrete integer method

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_1.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_1.m
@@ -1,15 +1,16 @@
 clear;
 clc;
 
-while 1==1
-    disp('Coin Flip Simulator');
-    number_toss = input('Enter number of tosses to be performed: ')  
+disp('Coin Flip Simulator');
+n = input('Enter number of tosses to be performed: ')  
     
-    results = randi([0:1], [number_toss,1]);
-    heads = sum(results(:,1)==0);
-    tails = sum(results(:,1)==1);
-    sum = heads + tails;
-    fprintf('Number of heads = %d\nNumber of tails = %d\n', heads, tails);
-    fprintf('Total = %d\n', sum);    
-    clear;
+results = zeros(n,4); % Preallocate an array 
+for i = 1:n
+    toss = randi([0:1], [i,1]);
+    results(i,1) = i;
+    results(i,2) = sum(toss(:,1)==0); % Count heads ( ==0)
+    results(i,3) = sum(toss(:,1)==1); % Count tails ( ==1)
+    results(i,4) = results(i,3)/i;
 end
+
+scatter(results(:,1),results(:,4))
