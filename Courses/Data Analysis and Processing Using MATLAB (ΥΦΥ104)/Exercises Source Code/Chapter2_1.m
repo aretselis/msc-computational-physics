89635edd897a0c3eb1a69363d34297b19fa0df9c
retse <anast.rets@hotmail.com>
2020-10-27 16:43:07 +0200

    Optimize code and include both scenarios

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_1.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_1.m
@@ -4,36 +4,50 @@ clc;
 disp('Coin Flipping Simulator');
 n = input('Enter number of tosses to be performed: ');  
 %Preallocate vectors
-flips = zeros(n,1);
 tails = zeros(n,1);
-ratio = zeros(n,1);
 
 %Discrete Method (Heads=0, Tails=1)
+flips = randi([0,1],n,1); % Add an extra flip to previous
 for i=1:n
-    results = randi([0,1], [i,1]);
-    tails(i)= sum(results);
-    ratio(i)= tails(i)/i;
+%      flips = randi([0,1], [i,1]); % Flip on all iterations until i
+%      tails(i)= sum(flips); % Flip on all iterations until i
+    
+    % Use previous flips
+    if i==1
+        tails(1)= flips(1);
+    else
+        tails(i) = tails(i-1) + flips(i);
+    end
 end
+ratio = tails./(1:n)';
 % Plot results and reset array for next run
-figure
-scatter((1:n),ratio,'.')
-title({'Coin flips from discrete method (0=Heads, 1=Tails)',''})
-xlabel('Number of flips, n')
-ylabel('Number of Heads/Total Flips')
-grid on
-results = zeros(n,1); % Reset results array
+figure;
+scatter((1:n),ratio,'.');
+title({'Coin flips from discrete method (0=Heads, 1=Tails)',''});
+xlabel('Number of flips, n');
+ylabel('Number of Heads/Total Flips');
+grid on;
 
 % Continuous Distribution in [0,1] (Heads<0.5, Tails>0.5)
-for i=1:n
-    results = round(rand(i,1));
-    tails(i)= sum(results);
-    ratio(i)= tails(i)/i;
-end
-% Plot results
-figure
-scatter(1:n,ratio,'.','r')
-title({'Coin flips from continuous distribution in [0,1]','(Heads if <0.5, Tails if >0.5)',''})
-xlabel('Number of flips, n')
-ylabel('Number of Heads/Total Flips')
-grid on
 
+flips=round(rand(1,n)); % Use previous flips
+for i=1:n
+%     flips = round(rand(i,1)); % Flip on all iterations until i
+%     tails(i)= sum(flips); % Flip on all iterations until i
+    
+    % Use previous flips
+    if i==1
+        tails(1)= flips(1);
+    else
+        tails(i) = tails(i-1) + flips(i);
+    end
+end
+ratio = tails./(1:n)';
+% Plot results
+figure;
+scatter(1:n,ratio,'.','r');
+title({'Coin flips from continuous distribution in [0,1]'...
+'(Heads if <0.5, Tails if >0.5)',''});
+xlabel('Number of flips, n');
+ylabel('Number of Heads/Total Flips');
+grid on;
