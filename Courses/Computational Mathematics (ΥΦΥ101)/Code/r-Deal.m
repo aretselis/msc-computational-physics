07579e0104cc062d47166fb96a8cddec41735ad9
Anastasios-Faidon Retselis <anast.rets@hotmail.com>
2020-10-29 21:33:27 +0000

    Include comments

--- Courses/Computational Mathematics (ΥΦΥ101)/Code/r-Deal.m
+++ Courses/Computational Mathematics (ΥΦΥ101)/Code/r-Deal.m
@@ -1,13 +1,14 @@
 clear;
 clc;
 
+% Input winning and simulations number
 disp('Deal 23rd box simulator');
 money = input('Enter your winnings: ');
 n = input('Enter number of simulations to be performed: ');
 results=zeros(n,1);
 
+% Compute
 extra_box = randi([1,5],n,1);
-
 for i=1:n
     if extra_box(i)==1
         results(i)= 0;
@@ -21,14 +22,13 @@ for i=1:n
         results(i)= 2*money;
     end
 end
-
 visualize=cumsum(results)./(1:n)';
 cashout = sum(results)/n;
 
+% Plot data and print results
 plot(1:n,visualize);
 title('Προσομοιωτής 23ου κουτιού');
 xlabel('Αριθμός προσομοίωσης, n');
 ylabel('Μέσο κέρδος, Ευρώ');
 grid on;
-
 fprintf('With winning box being %d euros, after %d simulations, you would win an average of %.2f euros\n', money, n, cashout);
