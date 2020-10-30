54af44664fbeacf7c19b9555d77b54c9e3b80cfc
retse <anast.rets@hotmail.com>
2020-10-30 11:23:43 +0200

    Use scarborough as stopping condition for bisection_root_finder

--- Courses/Computational Mathematics (ΥΦΥ101)/Code/2-bisection_root_finder.c
+++ Courses/Computational Mathematics (ΥΦΥ101)/Code/2-bisection_root_finder.c
@@ -4,7 +4,7 @@
 double f(double x);
 
 int main(){
-	double xl, xu, xr, es,value=1, value_prev=0;
+	double xl, xu, xr, es, ea, value=1, value_prev=0;
 	int n;
 	
 	printf("Enter upper value: ");
@@ -15,25 +15,32 @@ int main(){
 	printf("Enter the number of significant digits to be computed (Scarborough criterion): ");
 	scanf("%d", &n);
 	es=(0.5*pow(10,(2-n)));
+	printf("Scarborough criterion es=%lf %%\n\n", es);
 	
 	while(1){
 		if(f(xl)*f(xr)<0){
 			xu=xr;
+			value=xr;
 		}
 		else if(f(xl)*f(xr)>0){
 			xl=xr;
+			value=xr;
 		}
-		else if(f(xl)*f(xr)==0){
+		/* Use scarborough as stopping condition */
+		ea=fabs((value-value_prev)*100/value);
+		/* printf("Current ea equals%lf", ea);*/ 
+		if(ea<es){ /* if approximation error ea below es stop computing */
 			break;
-		}
+		} 
+		value_prev=xr;
 		xr=(xl+xu)/2;
-		printf("\nxr equals %lf", xr);
+		/* printf("\nxr equals %lf\n", xr); */
 	}
-	printf("\nThe root in given interval is equal to %lf", xr);
+	printf("\n\nThe root in given interval is equal to %lf", xr);
 	
 	return 0;
 }
 
 double f(double x){
-	return ((9.8*69.1*(1-exp(-x*10/68.1))/x)-40);
+	return ((9.8*68.1*(1-exp(-x*10/68.1))/x)-40);
 }
