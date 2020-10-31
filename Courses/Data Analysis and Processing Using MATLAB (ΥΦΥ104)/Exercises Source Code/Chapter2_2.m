2c7c5bd6e6fa6443c3d56dc09ff0ea849a90019b
retse <anast.rets@hotmail.com>
2020-10-31 19:11:53 +0200

    Include implementation with exprnd()

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_2.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_2.m
@@ -5,8 +5,19 @@ n = 1000;
 lambda = 1;
 syms x;
 
+% Using rand
+
 temp=rand(1,n);
 results=-(1/lambda)*log(1-temp);
+figure;
 hold on;
-histogram(results,'Normalization','pdf');
+histogram(results,70,'Normalization','pdf');
 fplot(lambda*exp(-lambda*x),[0,5]);
+
+% MATLAB built-in implementation
+
+% figure;
+% hold on;
+% results=exprnd(1,n);
+% histogram(results,'Normalization','pdf');
+% fplot(lambda*exp(-lambda*x),[0,5]);
