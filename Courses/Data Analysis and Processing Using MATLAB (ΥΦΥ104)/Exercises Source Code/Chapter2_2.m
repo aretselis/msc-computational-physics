f48c9df5f0f6d7c4425b896ee5a486c094e9df32
retse <anast.rets@hotmail.com>
2020-10-31 18:57:02 +0200

    Initial revision of Chapter2_2.m

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_2.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_2.m
@@ -1,3 +1,12 @@
 clear;
 clc;
 
+n = 1000;
+lambda = 1;
+syms x;
+
+temp=rand(1,n);
+results=-(1/lambda)*log(1-temp);
+hold on;
+histogram(results,'Normalization','pdf');
+fplot(lambda*exp(-lambda*x),[0,5]);
