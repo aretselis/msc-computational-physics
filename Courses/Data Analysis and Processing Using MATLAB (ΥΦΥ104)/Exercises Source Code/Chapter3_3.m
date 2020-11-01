cc20e7562ad3ff29926d48046defd2cddc7085d8
retse <anast.rets@hotmail.com>
2020-11-01 23:18:32 +0200

    Solve using mvnrnd()

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_3.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_3.m
@@ -1,15 +1,18 @@
 clear;
 clc;
 
-n=10000000;
-mu1=[2 0];
-mu2=[4 0];
+n=1000000;
+mu1=[0 1];
+mu2=[0 1];
+mu3=mu1+mu2;
 Sigma=[1 0];
 
 
 x = mvnrnd(mu1,Sigma,n);
 y = mvnrnd(mu2,Sigma,n);
+z = mvnrnd(mu3,Sigma,n);
 var(x)+var(y)
-var(x+y)
+var(z)
+
 
 
