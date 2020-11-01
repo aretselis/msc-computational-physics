2ab0f5a29f562d42778e86d709b47e43a4e5d07c
retse <anast.rets@hotmail.com>
2020-11-01 23:40:38 +0200

    Include correct Var[X+Y] calculation

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_3.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_3.m
@@ -2,17 +2,23 @@ clear;
 clc;
 
 n=1000000;
-mu1=[0 1];
-mu2=[0 1];
-mu3=mu1+mu2;
-Sigma=[1 0];
-
+mu1=1;
+Sigma=2;
 
 x = mvnrnd(mu1,Sigma,n);
-y = mvnrnd(mu2,Sigma,n);
-z = mvnrnd(mu3,Sigma,n);
+y = mvnrnd(mu1,Sigma,n);
 var(x)+var(y)
-var(z)
+var(x+y)
+
+% Correct Var[x+y]
+
+% actual = var(x)+var(y)+2*cov(x,y);
+% actual(2)
+
+
+
+
+
 
 
 
