b6c63055f49f8192e82ea8e1cacd4eab6b7f3731
retse <anast.rets@hotmail.com>
2020-11-15 12:52:02 +0200

    Restore Chapter2_3.m

---
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter2_3.m
@@ -0,0 +1,17 @@
+clear;
+clc;
+
+n=1000000;
+mu1=1;
+Sigma=2;
+
+x = mvnrnd(mu1,Sigma,n);
+y = mvnrnd(mu1,Sigma,n);
+var(x)+var(y)
+var(x+y)
+
+% Correct Var[x+y]
+
+% actual = var(x)+var(y)+2*cov(x,y);
+% actual(2)
+
