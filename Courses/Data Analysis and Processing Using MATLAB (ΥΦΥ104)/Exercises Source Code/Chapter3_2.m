d27d18543093c5a9362ffb771f352cac06062081
retse <anast.rets@hotmail.com>
2020-11-15 12:56:24 +0200

    Solve using exprnd()

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_2.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_2.m
@@ -1,2 +1,16 @@
 clear;clc;
 
+M = input('Enter number of samples M: ');
+n = input('Enter size of each sample n: ');
+tau = input('Enter tau parameter: ');
+
+sim = exprnd(tau, n, M);
+mean_sim = mean(sim);
+figure;
+histogram(mean_sim);
+title({['Histogram of mean values for ' num2str(M) ' samples with si'...
+    'ze ' num2str(n)] [' for an exponential distribution with'...
+    ' \tau=' num2str(tau)]});
+xlabel('Sample mean')
+ylabel('Counts');
+fprintf('Average of every sample mean is %.4f. \n',mean(mean_sim));
