5c8e18288b7824d8f375417f7118146031612fb6
retse <anast.rets@hotmail.com>
2020-11-15 13:04:23 +0200

    Include headers

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_1.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_1.m
@@ -1,9 +1,14 @@
+% Data Analysis and Processing using MATLAB (ΥΦΥ104)
+% Solution for exercise Chapter3_1
+% Implemented by Anastasis Retselis 
 clear;clc;
 
+% User input
 M = input('Enter number of samples M: ');
 n = input('Enter size of each sample n: ');
 lambda = input('Enter lambda parameter: ');
 
+% Results and histogram
 sim = poissrnd(lambda, n, M);
 mean_sim = mean(sim);
 figure;
--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_2.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_2.m
@@ -1,9 +1,14 @@
+% Data Analysis and Processing using MATLAB (ΥΦΥ104)
+% Solution for exercise Chapter3_2
+% Implemented by Anastasis Retselis
 clear;clc;
 
+% User input
 M = input('Enter number of samples M: ');
 n = input('Enter size of each sample n: ');
 tau = input('Enter tau parameter: ');
 
+% Results and diagram
 sim = exprnd(tau, n, M);
 mean_sim = mean(sim);
 figure;
