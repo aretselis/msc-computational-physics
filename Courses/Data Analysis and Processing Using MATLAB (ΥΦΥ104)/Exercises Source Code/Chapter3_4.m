c0b32847229e3ea2c3ab7876f2eb257f28eeea75
retse <anast.rets@hotmail.com>
2020-11-23 23:07:04 +0200

    Fix CI calculaton and check 5kV deviation assumption

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_4.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_4.m
@@ -8,7 +8,10 @@ data = [41 46 47 47 48 50 50 50 50 50 50 50 ...
     50 50 50 50 52 52 53 53 53 53 53 57 ...
     52 52 53 53 53 53 53 53 54 54 55 68];
 
-% Calculate 95% confidence interval
+%% Calculate 95% confidence interval and check 5kV deviation assumption
+sd=5;
 
-[~,~,ci,~] = ttest(data);
-fprintf('The 95%% confidence interval is [%.4f,%.4f]', ci(1), ci(2));
+[~,p,ci,~] = vartest(data,sd^2,1-0.95);
+fprintf('The 95%% confidence interval for the variance is [%.4f,%.4f]\n', ci(1), ci(2));
+fprintf('The 95%% confidence inteval for the standard deviation is [%.4f,%.4f]\n', sqrt(ci(1)), sqrt(ci(2)));
+fprintf('p-value for standard deviation σ=%d is %1.4f', sd, p);
