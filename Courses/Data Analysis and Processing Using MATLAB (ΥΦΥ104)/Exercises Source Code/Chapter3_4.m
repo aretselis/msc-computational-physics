acc75252580d1340b5d968e8c16065389600b344
retse <anast.rets@hotmail.com>
2020-11-23 20:51:27 +0200

    Calculate 95% confidence interval

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_4.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_4.m
@@ -8,3 +8,7 @@ data = [41 46 47 47 48 50 50 50 50 50 50 50 ...
     50 50 50 50 52 52 53 53 53 53 53 57 ...
     52 52 53 53 53 53 53 53 54 54 55 68];
 
+% Calculate 95% confidence interval
+
+[~,~,ci,~] = ttest(data);
+fprintf('The 95%% confidence interval is [%.4f,%.4f]', ci(1), ci(2));
