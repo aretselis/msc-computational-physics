5c5ef956c7dc4858f92dbeeae01a7b8900891ac9
retse <anast.rets@hotmail.com>
2020-11-29 10:16:12 +0200

    Perform goodness-of-fit test

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_4.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_4.m
@@ -8,23 +8,39 @@ data = [41 46 47 47 48 50 50 50 50 50 50 50 ...
     50 50 50 50 52 52 53 53 53 53 53 57 ...
     52 52 53 53 53 53 53 53 54 54 55 68];
 
-histfit(data, 7);
-
+% histfit(data);
 
 % Calculate 95% confidence interval for variance and check 5kV deviation assumption
 
 sd=5;
-[~,p_var,ci_var,~] = vartest(data,sd^2,0.05);
+[h,p_var,ci_var,~] = vartest(data,sd^2,0.05);
 fprintf('The 95%% confidence interval for the variance is [%.4f,%.4f]\n', ci_var(1), ci_var(2));
 fprintf('The 95%% confidence inteval for the standard deviation is [%.4f,%.4f]\n', sqrt(ci_var(1)), sqrt(ci_var(2)));
-fprintf('p-value for standard deviation σ=%d is %1.4f\n\n', sd, p_var);
+fprintf('p-value for standard deviation σ=%d is %1.4f\n', sd, p_var);
+if h==0
+    fprintf('Hypothesis for standard deviation true\n\n');
+elseif h==1
+    fprintf('Hypothesis for standard deviation false\n\n');
+end
 
 % Calculate 95% confidence interval for cut-off voltage and estimate 52 kV
 
 cutoff_voltage=52;
-[~,p_cv,ci_cv,~] = ttest(data, cutoff_voltage, 0.05);
+[h,p_cv,ci_cv,~] = ttest(data, cutoff_voltage, 0.05);
 fprintf('The 95%% confidence interval for the cut-off voltage is [%.4f,%.4f]\n', ci_cv(1), ci_cv(2));
-fprintf('p-value for cut-off voltage V=%d is %1.4f\n\n', cutoff_voltage, p_cv);
+fprintf('p-value for cut-off voltage V=%d is %1.4f\n', cutoff_voltage, p_cv);
+if h == 0
+    fprintf('Hypothesis for cut-off voltage true\n\n');
+elseif h==1
+    fprintf('Hypothesis for cut-off voltage false\n\n');
+end
 
 % Goodness-of-fit test
 
+[h, p_fit] = chi2gof(data);
+fprintf('p-value for goodness-of-fit for normal distribution is %1.5f\n', p_fit);
+if h == 0
+    fprintf('Hypothesis for goodness-of-fit for normal distribution true\n\n');
+elseif h==1
+    fprintf('Hypothesis for goodness-of-fit for normal distribution false\n\n');
+end
