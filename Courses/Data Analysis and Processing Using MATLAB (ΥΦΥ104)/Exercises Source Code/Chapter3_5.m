5a1effb3f4e97576b563521663645650a587f85c
retse <anast.rets@hotmail.com>
2020-11-29 11:22:58 +0200

    Find ci for all datasets, perform p-value checks and run goodness-of-fit checks

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_5.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter3_5.m
@@ -12,15 +12,95 @@ data = importdata('eruption.dat');
 % Check confidence interval for standard deviation
 sd_waiting = 10; 
 sd_eruption = 1;
+mv_waiting = 75;
+mv_eruption = 2.5;
 
 %% Column 1: Waiting time (minutes) 1989
 
-fprintf('Stats for Waiting time in 1989:\n');
-[h, p_wait, ci_wait] = vartest(data(:,1),sd_waiting^2,0.05);
-fprintf('Confidence interval for standard deviation (σ) is [%.4f,%.4f]\n', sqrt(ci_wait(1)), sqrt(ci_wait(2)));
-fprintf('p-value for hypothesis σ = %d is %.4f\n', sd_waiting, p_wait);
+fprintf('<strong>Stats for Waiting time in 1989:\n</strong>');
+fprintf('========================================\n');
+[h, p_waiting, ci_waiting] = vartest(data(:,1),sd_waiting^2,0.05);
+fprintf('Confidence interval for standard deviation (σ) is [%.4f,%.4f]\n', sqrt(ci_waiting(1)), sqrt(ci_waiting(2)));
+fprintf('p-value for hypothesis σ = %d is %.5f\n', sd_waiting, p_waiting);
 if h == 0
-    fprintf('Hypothesis for σ true\n\n');
+    fprintf('Hypothesis for σ true\n');
 elseif h==1
-    fprintf('Hypothesis for σ false\n\n');
+    fprintf('Hypothesis for σ false\n');
+end
+fprintf('========================================\n');
+[h, p_waiting, ci_waiting] = ttest(data(:,1),mv_waiting,0.05);
+fprintf('Confidence interval for mean value of waiting time is [%.4f,%.4f]\n', ci_waiting(1), ci_waiting(2));
+fprintf('p-value for hypothesis mv = %d is %.5f\n', mv_waiting, p_waiting);
+if h == 0
+    fprintf('Hypothesis for mv true\n');
+elseif h==1
+    fprintf('Hypothesis for mv false\n');
+end
+fprintf('========================================\n');
+[h, p_fit] = chi2gof(data(:,1));
+fprintf('p-value for goodness-of-fit for normal distribution is %.5f\n', p_fit);
+if h == 0
+    fprintf('Hypothesis for goodness-of-fit for normal distribution true\n\n');
+elseif h==1
+    fprintf('Hypothesis for goodness-of-fit for normal distribution false\n\n');
+end
+
+%% Column 2: Eruption time (minutes) 1989
+
+fprintf('<strong>Stats for Eruption time in 1989:\n</strong>');
+fprintf('========================================\n');
+[h, p_eruption, ci_eruption] = vartest(data(:,2),sd_eruption^2,0.05);
+fprintf('Confidence interval for standard deviation (σ) is [%.4f,%.4f]\n', sqrt(ci_eruption(1)), sqrt(ci_eruption(2)));
+fprintf('p-value for hypothesis σ = %d is %.5f\n', sd_eruption, p_eruption);
+if h == 0
+    fprintf('Hypothesis for σ true\n');
+elseif h==1
+    fprintf('Hypothesis for σ false\n');
+end
+fprintf('========================================\n');
+[h, p_eruption, ci_eruption] = ttest(data(:,2),mv_eruption,0.05);
+fprintf('Confidence interval for mean value of eruption time is [%.4f,%.4f]\n', ci_eruption(1), ci_eruption(2));
+fprintf('p-value for hypothesis mv = %.2f is %.5f\n', mv_eruption,p_eruption);
+if h == 0
+    fprintf('Hypothesis for mv true\n');
+elseif h==1
+    fprintf('Hypothesis for mv false\n');
+end
+fprintf('========================================\n');
+[h, p_fit] = chi2gof(data(:,2));
+fprintf('p-value for goodness-of-fit for normal distribution is %.5f\n', p_fit);
+if h == 0
+    fprintf('Hypothesis for goodness-of-fit for normal distribution true\n\n');
+elseif h==1
+    fprintf('Hypothesis for goodness-of-fit for normal distribution false\n\n');
+end
+
+%% Column 3: Waiting time (minutes) 2006
+
+fprintf('<strong>Stats for Waiting time in 2006:\n</strong>');
+fprintf('========================================\n');
+[h, p_waiting, ci_waiting] = vartest(data(:,3),sd_waiting^2,0.05);
+fprintf('Confidence interval for standard deviation (σ) is [%.4f,%.4f]\n', sqrt(ci_waiting(1)), sqrt(ci_waiting(2)));
+fprintf('p-value for hypothesis σ = %d is %.5f\n', sd_waiting, p_waiting);
+if h == 0
+    fprintf('Hypothesis for σ true\n');
+elseif h==1
+    fprintf('Hypothesis for σ false\n');
+end
+fprintf('========================================\n');
+[h, p_waiting, ci_waiting] = ttest(data(:,3),mv_waiting,0.05);
+fprintf('Confidence interval for mean value of waiting time is [%.4f,%.4f]\n', ci_waiting(1), ci_waiting(2));
+fprintf('p-value for hypothesis mv = %d is %.5f\n', mv_waiting, p_waiting);
+if h == 0
+    fprintf('Hypothesis for mv true\n');
+elseif h==1
+    fprintf('Hypothesis for mv false\n');
+end
+fprintf('========================================\n');
+[h, p_fit] = chi2gof(data(:,3));
+fprintf('p-value for goodness-of-fit for normal distribution is %.5f\n', p_fit);
+if h == 0
+    fprintf('Hypothesis for goodness-of-fit for normal distribution true\n\n');
+elseif h==1
+    fprintf('Hypothesis for goodness-of-fit for normal distribution false\n\n');
 end
