e450e3fcdcc0c4a82b088c51b03ef5aecc540f9b
retse <anast.rets@hotmail.com>
2020-12-03 11:30:48 +0200

    Implement vectorized simulation of experiment for M times

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter4_1.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter4_1.m
@@ -3,3 +3,61 @@
 % Implemented by Anastasis Retselis
 clear;clc;
 
+% e=sqrt(h2/h1), where
+% h1=initial height
+% h2=height after first contact with ground
+
+%% A) 5 given repetitions
+
+h1 = 100;
+h2 = [60 54 58 60 56];
+
+e = sqrt(h2/h1);
+sigma_e = std(e);
+
+
+%% B) Simulate 1000 times the experiment in A)
+
+M = 1000;
+n = 5;
+mv_h2 = 58;
+sigma_h2 = 2;
+
+% Create simulation
+
+simulation = normrnd(mv_h2, sigma_h2, n, M);
+sim_mean_h2 = mean(simulation);
+sim_sigma_h2 = std(simulation);
+e = sqrt(simulation/h1);
+sim_mean_e = mean(e);
+sim_sigma_e = std(e);
+
+% Plot results
+
+figure;
+histogram(sim_mean_h2);
+title("Mean value for h_2 for M = " + M + " experiments");
+xlabel('Mean value, \mu_{h_2}, [cm]');
+ylabel('Counts');
+grid on;
+
+figure;
+histogram(sim_sigma_h2);
+title("Standard deviation for h_2 for M = " + M + " experiments");
+xlabel('Standard deviation, \sigma_{h_2}, [cm]');
+ylabel('Counts');
+grid on;
+
+figure;
+histogram(sim_mean_e);
+title("Mean value for e for M = " + M + " experiments");
+xlabel('Mean value, \mu_{e}, [ ]');
+ylabel('Counts');
+grid on;
+
+figure;
+histogram(sim_sigma_e);
+title("Standard deviation for e for M = " + M + " experiments");
+xlabel('Standard deviation, \sigma_{e}, [ ]');
+ylabel('Counts');
+grid on;
