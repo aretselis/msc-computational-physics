2743a5d0f8510131c827cdf39809daa05e86a0af
retse <anast.rets@hotmail.com>
2020-12-03 22:10:50 +0200

    Complete simulation with M runs and plot results

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter4_3.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter4_3.m
@@ -14,9 +14,33 @@ sigma_I = 0.071;    % A
 mean_f = 0.283;          % rad
 sigma_f = 0.017;    % rad
 
-sigma_p = sqrt((mean_I*cos(mean_f)*sigma_V)^2 + (mean_V*cos(mean_f)*...
-    sigma_I)^2 + (-mean_V*mean_I*sin(mean_f))^2); 
+sigma_P = sqrt((mean_I*cos(mean_f)*sigma_V)^2 + (mean_V*cos(mean_f)*...
+    sigma_I)^2 + (-mean_V*mean_I*sin(mean_f)*sigma_f)^2); 
+mean_P = mean_V*mean_I*cos(mean_f);
+fprintf('Based on initial values we have P =%.4f and σ_P = %.4f\n',...
+    mean_P, sigma_P);
 
 % Create and run simulation
 
-M = 100;
+M = 1000;
+sim_mean_V = normrnd(mean_V, sigma_V, 1, M);
+sim_mean_I = normrnd(mean_I, sigma_I, 1, M);
+sim_mean_f = normrnd(mean_f, sigma_f, 1, M);
+sim_P = sim_mean_V.*sim_mean_I.*cos(sim_mean_f);
+sim_sigma_P = std(sim_P);
+sim_mean_P = mean(sim_P);
+fprintf('For M=%d runs, the power is P = %.4f with σ_P = %.4f\n', M,...
+ sim_mean_P, sim_sigma_P);
+
+% Plot results
+
+figure;
+hold on;
+x1 = xline(mean_P,'-r','LineWidth',1);
+histogram(sim_P);
+legend("Expected \mu_{P} = " + num2str(mean_P,"%1.4f"));
+title("Mean value \mu for P for M = " + M + " experiments");
+xlabel('Mean dissipated power, \mu_{P}, [Watt]');
+ylabel('Counts');
+grid on;
+
