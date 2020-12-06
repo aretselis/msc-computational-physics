b7a6654ea6f6bb00104867377dfeec72c989fff6
retse <anast.rets@hotmail.com>
2020-12-06 14:12:38 +0200

    Include correlation between V and f and minor fixes

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter4_3.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter4_3.m
@@ -9,7 +9,7 @@ clear;clc;close all;
 
 mean_V = 77.78;     % V
 sigma_V = 0.71;     % V
-mean_I = 1.12;      % A
+mean_I = 1.21;      % A
 sigma_I = 0.071;    % A
 mean_f = 0.283;          % rad
 sigma_f = 0.017;    % rad
@@ -17,7 +17,7 @@ sigma_f = 0.017;    % rad
 sigma_P = sqrt((mean_I*cos(mean_f)*sigma_V)^2 + (mean_V*cos(mean_f)*...
     sigma_I)^2 + (-mean_V*mean_I*sin(mean_f)*sigma_f)^2); 
 mean_P = mean_V*mean_I*cos(mean_f);
-fprintf('Based on initial values we have P =%.4f and σ_P = %.4f\n',...
+fprintf('Based on initial values we have P = %.4f and σ_P = %.4f\n',...
     mean_P, sigma_P);
 
 % Create and run simulation
@@ -29,7 +29,7 @@ sim_mean_f = normrnd(mean_f, sigma_f, 1, M);
 sim_P = sim_mean_V.*sim_mean_I.*cos(sim_mean_f);
 sim_sigma_P = std(sim_P);
 sim_mean_P = mean(sim_P);
-fprintf('For M=%d runs, the power is P = %.4f with σ_P = %.4f\n', M,...
+fprintf('For M = %d runs, the power is P = %.4f with σ_P = %.4f\n', M,...
  sim_mean_P, sim_sigma_P);
 
 % Plot results
@@ -44,3 +44,27 @@ xlabel('Mean dissipated power, \mu_{P}, [Watt]');
 ylabel('Counts');
 grid on;
 
+%% C) Include ρ_vf in simulation
+
+% One extra term for sigma
+rho_vf = 0.5;
+sigma_P_vf = sqrt((mean_I*cos(mean_f)*sigma_V)^2 + (mean_V*cos(mean_f)*...
+    sigma_I)^2 + (-mean_V*mean_I*sin(mean_f)*sigma_f)^2 + 2*mean_I*...
+    cos(mean_f)*-mean_V*mean_I*sin(mean_f)*rho_vf*sigma_V*sigma_f);
+
+
+mean_vector = [mean_V mean_I mean_f];
+cov_matrix = [sigma_V^2 0 0;...
+              0 sigma_I^2 0;...
+              0 0 sigma_f^2];
+sim_vf = mvnrnd(mean_vector, cov_matrix, M);
+mean_V_vf = sim_vf(:,1);
+mean_I_vf = sim_vf(:,2);
+mean_f_vf = sim_vf(:,3);
+sim_vf_p = mean_V_vf.*mean_I_vf.*cos(mean_f_vf);
+sim_vf_sigma_P = std(sim_vf_p);
+sim_vf_mean_P = mean(sim_vf_p);
+
+fprintf(['Taking ρ_vf = %.2f into account...\nFor M = %d runs, the',...
+    ' power is P = %.4f with σ_P = %.4f (expected σ_P = %.4f)\n'],...
+    rho_vf, M, sim_vf_mean_P, sim_vf_sigma_P, sigma_P_vf);
