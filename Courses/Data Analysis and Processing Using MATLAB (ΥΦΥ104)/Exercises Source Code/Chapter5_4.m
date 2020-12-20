beeb96b8f5a0f231d75e3b15553675f799a8109b
retse <anast.rets@hotmail.com>
2020-12-20 12:20:31 +0200

    Compuute correlation coefficient and plot data

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_4.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_4.m
@@ -2,3 +2,19 @@
 % Solution for exercise Chapter5_4
 % Implemented by Anastasis Retselis
 clear;clc;close all;
+
+lightairdata = importdata('lightair.dat');
+measurements_count = length(lightairdata);
+density = lightairdata(:,1);
+light_speed = lightairdata(:,2);
+
+lightair_corr_mat = corrcoef(lightairdata);
+lightair_corrcoef = lightair_corr_mat(2);
+figure;
+plot(density,light_speed,'.');
+title(sprintf('Light Speed as a function of air density (r = %.4f)',...
+    lightair_corrcoef));
+xlabel('Air Density, ρ, [kg/m^3]')
+ylabel('Light Speed, c_{air}, [m/s]')
+grid on;
+
