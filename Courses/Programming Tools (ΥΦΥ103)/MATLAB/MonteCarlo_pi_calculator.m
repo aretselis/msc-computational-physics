1fa81ddc00d8cb49069a01c83e1579e5e3cea967
retse <anast.rets@hotmail.com>
2021-01-02 13:25:26 +0200

    Compute simulation error and print results

--- Courses/Programming Tools (ΥΦΥ103)/MATLAB/MonteCarlo_pi_calculator.m
+++ Courses/Programming Tools (ΥΦΥ103)/MATLAB/MonteCarlo_pi_calculator.m
@@ -4,16 +4,23 @@ n = 100000;
 radius = 1;
 max = radius;
 min = -radius;
-length = radius;
+cube_length = radius;
 
 % The code below is completely vectorized, meaning it does not use any
 % actual loop structures, in order to fully utilize MATLAB to achieve
 % faster speeds
 
-coordinates = min + (max-min).*rand(3,n);
-r = sqrt(coordinates(1,:).^2+coordinates(2,:).^2+coordinates(3,:).^2);
+vector_coordinates = min + (max-min).*rand(3,n);
+r = sqrt(vector_coordinates(1,:).^2+vector_coordinates(2,:).^2+...
+    vector_coordinates(3,:).^2);
 r_inside = r <= radius;
 n_inside_sphere = sum(r_inside);
 
 pi_approx = 6*n_inside_sphere/n;
-pi_approx
+pi_error = 6*std(r_inside)/sqrt(length(r));
+pi_lower = pi_approx - pi_error;
+pi_upper = pi_approx + pi_error;
+
+fprintf('π_approx = %.5f\n', pi_approx);
+fprintf('error = %.5f\n', pi_error);
+fprintf('Actual π will be inside [%.5f, %.5f]\n', pi_lower, pi_upper);
