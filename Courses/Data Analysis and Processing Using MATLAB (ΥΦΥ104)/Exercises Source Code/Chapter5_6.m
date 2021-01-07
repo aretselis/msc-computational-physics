61f7cbc54ae559d6f3571c8bdaaa4e40e93a852c
retse <anast.rets@hotmail.com>
2021-01-07 14:03:17 +0200

    Add Chapter5_6.m

---
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_6.m
@@ -0,0 +1,13 @@
+% Data Analysis and Processing using MATLAB (ΥΦΥ104)
+% Solution for exercise Chapter5_6
+% Implemented by Anastasis Retselis
+clear;clc;close all;
+
+tyre_data = [2 98.2
+        3 91.7
+        8 81.3
+        16 64.0
+        32 36.4
+        48 32.6
+        64 17.1
+        80 11.33];
