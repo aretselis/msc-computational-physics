82228d261a11caccb57c6aac7b8da82b989ca827
retse <anast.rets@hotmail.com>
2021-01-15 20:08:30 +0200

    Add fourth degree and plot fitting polynomials

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_7.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_7.m
@@ -48,14 +48,16 @@ k = 1;
 first_degree_X = [ones(size) resistance_data];
 [b,bint,r,rint,stats] = regress(temperature_data, first_degree_X);
 y_regression_first = first_degree_X * b;
-diagnostic_plot(temperature_data, y_regression_first, n, k);
+diagnostic_plot(temperature_data, y_regression_first,...
+   resistance_data, n, k);
 
 % Second degree
 k = 2;
 second_degree_X = [ones(size) resistance_data resistance_data.^2];
 [b,bint,r,rint,stats] = regress(temperature_data, second_degree_X);
 y_regression_second = second_degree_X * b;
-diagnostic_plot(temperature_data, y_regression_second, n, k);
+diagnostic_plot(temperature_data, y_regression_second,...
+   resistance_data, n, k);
 
 % Third degree
 k = 3;
@@ -63,18 +65,27 @@ third_degree_X = [ones(size) resistance_data resistance_data.^2 ...
     resistance_data.^3];
 [b,bint,r,rint,stats] = regress(temperature_data, third_degree_X);
 y_regression_third = third_degree_X * b;
-diagnostic_plot(temperature_data,y_regression_third, n, k);
+diagnostic_plot(temperature_data,y_regression_third,...
+   resistance_data, n, k);
+
+% Fourth degree (just for demo purposes)
+k = 4;
+fourth_degree_X = [ones(size) resistance_data resistance_data.^2 ...
+    resistance_data.^3 resistance_data.^4];
+[b,bint,r,rint,stats] = regress(temperature_data, fourth_degree_X);
+y_regression_fourth = fourth_degree_X * b;
+diagnostic_plot(temperature_data,y_regression_fourth,...
+   resistance_data, n, k);
 
 
-
-function diagnostic_plot(y_values, y_hat, n, k)
+function diagnostic_plot(y_values, y_hat, x_values, n, k)
     % Compute and plot diagnostic plot
     % Also add plot of fitting for clarity
     e_i = y_values - y_hat;
     s_e = sqrt(sum((y_values-y_hat).^2)/(n-k+1));
     e_i_star = e_i/s_e;
     % Diagnostic Plot
-    subplot(3,2,2*k)
+    subplot(4,2,2*k)
     hold on;
     grid on;
     yline(2,'-r')
@@ -83,4 +94,14 @@ function diagnostic_plot(y_values, y_hat, n, k)
     title('Diagnostic plot')
     xlabel('1/T')
     ylabel('e_i*')
+    % Fitting plot
+    subplot(4,2,2*k-1);
+    hold on;
+    grid on;
+    plot(x_values,y_values,'.')
+    plot(x_values,y_hat)
+    title(['Polynomial fit of degree = ', num2str(k)])
+    xlabel('ln(R)')
+    ylabel('1/T')
+    legend('Data Points', sprintf('degree=%d', k));
 end
