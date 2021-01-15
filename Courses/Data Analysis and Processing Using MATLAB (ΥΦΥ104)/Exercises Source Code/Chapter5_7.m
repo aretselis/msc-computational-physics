8c359725bfdfdcb5528e849fa42034c99acccb91
retse <anast.rets@hotmail.com>
2021-01-15 21:53:03 +0200

    Add Steinhart-Hart model and compare with third degree polynomial

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_7.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_7.m
@@ -41,21 +41,21 @@ temperature_data = 1./(data(:,3) + 273.15);
 n = length(resistance_data);
 size = size(resistance_data);
 
-plot(resistance_data,temperature_data,'.');
+%% Find degree of best polynomial fit
 
 % First degree
 k = 1;
 first_degree_X = [ones(size) resistance_data];
-[b,bint,r,rint,stats] = regress(temperature_data, first_degree_X);
-y_regression_first = first_degree_X * b;
+[b_1,bint,r,rint,stats] = regress(temperature_data, first_degree_X);
+y_regression_first = first_degree_X * b_1;
 diagnostic_plot(temperature_data, y_regression_first,...
    resistance_data, n, k);
 
 % Second degree
 k = 2;
 second_degree_X = [ones(size) resistance_data resistance_data.^2];
-[b,bint,r,rint,stats] = regress(temperature_data, second_degree_X);
-y_regression_second = second_degree_X * b;
+[b_2,bint,r,rint,stats] = regress(temperature_data, second_degree_X);
+y_regression_second = second_degree_X * b_2;
 diagnostic_plot(temperature_data, y_regression_second,...
    resistance_data, n, k);
 
@@ -63,8 +63,8 @@ diagnostic_plot(temperature_data, y_regression_second,...
 k = 3;
 third_degree_X = [ones(size) resistance_data resistance_data.^2 ...
     resistance_data.^3];
-[b,bint,r,rint,stats] = regress(temperature_data, third_degree_X);
-y_regression_third = third_degree_X * b;
+[b_3,bint,r,rint,stats] = regress(temperature_data, third_degree_X);
+y_regression_third = third_degree_X * b_3;
 diagnostic_plot(temperature_data,y_regression_third,...
    resistance_data, n, k);
 
@@ -72,11 +72,79 @@ diagnostic_plot(temperature_data,y_regression_third,...
 k = 4;
 fourth_degree_X = [ones(size) resistance_data resistance_data.^2 ...
     resistance_data.^3 resistance_data.^4];
-[b,bint,r,rint,stats] = regress(temperature_data, fourth_degree_X);
-y_regression_fourth = fourth_degree_X * b;
+[b_4,bint,r,rint,stats] = regress(temperature_data, fourth_degree_X);
+y_regression_fourth = fourth_degree_X * b_4;
 diagnostic_plot(temperature_data,y_regression_fourth,...
    resistance_data, n, k);
 
+fprintf(['Based on the diagnostic plots, we can conclude that the ',...
+    'best polynomial fit is of 3rd degree\n\nThe following equation',...
+    ' is to be used:\ny=\n']);
+for i=0:3
+    fprintf('%.9f x^%.d\n', b_3(i+1), i);
+end
+
+%% Compare third degree with Steinhart-Hart model
+
+% Plot third degree
+% Fitting plot
+figure;
+subplot(2,2,1);
+hold on;
+grid on;
+plot(resistance_data,temperature_data,'.')
+plot(resistance_data,y_regression_third)
+title('Polynomial fit of degree = 3')
+xlabel('ln(R)')
+ylabel('1/T')
+legend('Data Points', 'degree=3');
+% Diagnostic Plot
+e_i = temperature_data - y_regression_third;
+s_e = sqrt(sum((temperature_data-y_regression_third).^2)/(n-k+1));
+e_i_star = e_i/s_e;
+subplot(2,2,2)
+hold on;
+grid on;
+yline(2,'-r')
+yline(-2,'-r')
+plot(y_regression_third,e_i_star,'.');
+title('Diagnostic plot')
+xlabel('1/T')
+ylabel('e_i*')
+
+% Compute and plot Steinhart-Hart model
+
+steinhart_X = [ones(size) resistance_data resistance_data.^3];
+[b_s,bint,r,rint,stats] = regress(temperature_data, steinhart_X);
+y_regression_steinhart = steinhart_X * b_s;
+% Fitting plot
+subplot(2,2,3);
+hold on;
+grid on;
+plot(resistance_data,temperature_data,'.')
+plot(resistance_data,y_regression_steinhart)
+title('Steinhart-Hart model')
+xlabel('ln(R)')
+ylabel('1/T')
+legend('Data Points', 'Steinhart-Hart model');
+% Diagnostic Plot
+e_i = temperature_data - y_regression_steinhart;
+s_e = sqrt(sum((temperature_data-y_regression_steinhart).^2)/(n-k+1));
+e_i_star = e_i/s_e;
+subplot(2,2,4)
+hold on;
+grid on;
+yline(2,'-r')
+yline(-2,'-r')
+plot(y_regression_steinhart,e_i_star,'.');
+title('Diagnostic plot')
+xlabel('1/T')
+ylabel('e_i*')
+
+fprintf('\nSteinhart model coefficients are:\n');
+fprintf('b0 = %.9f\n', b_s(1));
+fprintf('b1 = %.9f\n', b_s(2));
+fprintf('b3 = %.9f\n', b_s(3));
 
 function diagnostic_plot(y_values, y_hat, x_values, n, k)
     % Compute and plot diagnostic plot
