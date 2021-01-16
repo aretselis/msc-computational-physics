785e8376aef32745847dddf34c24a70a25375d36
retse <anast.rets@hotmail.com>
2021-01-16 22:28:34 +0200

    Compute R^2 and adjR^2

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_8.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_8.m
@@ -22,14 +22,20 @@ head = 11;
 
 mass_data = data(:,1);
 data(:,1) = [];
+k = length(data(1,:));
 
 %% Model including all variables as linearz
 
 x_data_reg = [ones(n,1) data];
 [b_coefs,b_ints,~,~,~] = regress(mass_data, x_data_reg);
+y_regression = x_data_reg * b_coefs;
+R2_all = 1 - (sum((mass_data-y_regression).^2)/...
+    sum((mass_data-mean(mass_data)).^2));
+adjR2_all = 1-(sum((mass_data-y_regression).^2)/...
+    sum((mass_data-mean(mass_data)).^2)*(n-1)/(n-(k+1)));
 
 fprintf('Linear regression based on all variables:\n\n');
-fprintf('Variable\t\tb_coef\t\t95%% CI\n');
+fprintf('Variable\t\tb_coeff\t\t95%% Confidence Interval\n');
 fprintf('%8s\t\t%.3f\t\t[%.3f, %.3f]\n','Constant',b_coefs(1),...
     b_ints(1), b_ints(2));
 for i=2:length(headers)
@@ -38,4 +44,7 @@ for i=2:length(headers)
     fprintf('[%.3f, %.3f]\n', b_ints(i+1), b_ints(i+2));
 end
 
-stepwisefit(data,mass_data);
+fprintf('\nR^2\t   = %.4f', R2_all);
+fprintf('\nadjR^2 = %.4f', adjR2_all);
+
+% stepwisefit(data,mass_data);
