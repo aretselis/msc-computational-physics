b443d498f48696609b9511d2016977d8ab45207a
retse <anast.rets@hotmail.com>
2021-01-16 23:47:00 +0200

    Implement stepwise model and print results

--- Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_8.m
+++ Courses/Data Analysis and Processing Using MATLAB (ΥΦΥ104)/Exercises Source Code/Chapter5_8.m
@@ -7,30 +7,16 @@ physical = importdata('physical.txt','	',1);
 data = physical.data;
 headers = physical.colheaders;
 n = length(data);
-
-mass = 1;
-fore = 2;
-bicep = 3;
-chest = 4;
-neck = 5;
-shoulder =6;
-waist = 7;
-height = 8;
-calf = 9;
-thigh = 10;
-head = 11;
-
 mass_data = data(:,1);
 data(:,1) = [];
 k = length(data(1,:));
 
-%% Model including all variables as linearz
+%% Model including all variables as linear
 
 x_data_reg = [ones(n,1) data];
 [b_coefs,b_ints,~,~,~] = regress(mass_data, x_data_reg);
 y_regression = x_data_reg * b_coefs;
-e_i = mass_data-y_regression;
-s_e2_all = sum((mass_data-y_regression).^2)/(n-k+1);
+s_e2_all = sum((mass_data-y_regression).^2)/(n-(k+1));
 R2_all = 1 - (sum((mass_data-y_regression).^2)/...
     sum((mass_data-mean(mass_data)).^2));
 adjR2_all = 1-(sum((mass_data-y_regression).^2)/...
@@ -50,4 +36,38 @@ fprintf('\nVariance of e_i: σ_ε^2 = %.4f', s_e2_all);
 fprintf('\nR^2\t   = %.4f', R2_all);
 fprintf('\nadjR^2 = %.4f', adjR2_all);
 
-% stepwisefit(data,mass_data);
+%% Stepwise model
+
+fprintf('\n\nNow using a stepwise method...\n\n');
+[b_temp,se,~,value_check,stats] = stepwisefit(data,mass_data);
+b_step = [stats.intercept ;b_temp];
+a = 0.05;
+k_step = sum(value_check);
+t_critical = tinv(1-a/2,n-(k_step+1));
+ci_step = [se se];
+ci_step(:,1) = b_temp(:,1) - t_critical*se;
+ci_step(:,2) = b_temp(:,1) + t_critical*se;
+
+logic_test = [1 value_check]';
+coefs_step = (b_step .*logic_test); 
+y_regression_step = x_data_reg * coefs_step;  
+s_e2_step = sum((mass_data-y_regression_step).^2)/(n-(k_step+1));
+R2_step = 1 - (sum((mass_data-y_regression_step).^2)/...
+    sum((mass_data-mean(mass_data)).^2));
+adjR2_step = 1-(sum((mass_data-y_regression_step).^2)/...
+    sum((mass_data-mean(mass_data)).^2)*(n-1)/(n-(k_step+1)));
+
+
+fprintf('Stepwise model from MATLAB:\n\n');
+fprintf('Variable\t\tb_coeff\t\t95%% Confidence Interval\n');
+fprintf('%8s\t  %.3f\t\t\n','Constant',b_step(1));
+for i=2:length(headers)
+    if value_check(i-1)==1
+        fprintf('%8s\t\t', headers{i});
+        fprintf('%.4f\t\t', b_step(i));
+        fprintf('[%.4f, %.4f]\n', ci_step(i-1,1), ci_step(i-1,2));
+    end
+end
+fprintf('\nVariance of e_i: σ_ε^2 = %.4f', s_e2_step);
+fprintf('\nR^2\t   = %.4f', R2_step);
+fprintf('\nadjR^2 = %.4f\n', adjR2_step);
