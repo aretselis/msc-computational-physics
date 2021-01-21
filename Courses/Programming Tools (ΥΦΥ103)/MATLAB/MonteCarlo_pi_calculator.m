da139b5de860c957ffe3bc0247775a5290d89122
retse <anast.rets@hotmail.com>
2021-01-21 17:19:28 +0200

    Remove unused variables

--- Courses/Programming Tools (ΥΦΥ103)/MATLAB/MonteCarlo_pi_calculator.m
+++ Courses/Programming Tools (ΥΦΥ103)/MATLAB/MonteCarlo_pi_calculator.m
@@ -7,13 +7,11 @@ n = 1000000;
 radius = 1;
 max = radius;
 min = -radius;
-cube_length = radius;
 
 % The code below is completely vectorized, meaning it does not use any
 % actual loop structures, in order to fully utilize MATLAB to achieve
 % faster speeds
 
-tic;
 vector_coordinates = min + (max-min).*rand(3,n);
 r = sqrt(vector_coordinates(1,:).^2+vector_coordinates(2,:).^2+...
     vector_coordinates(3,:).^2);
@@ -24,7 +22,6 @@ pi_approx = 6*n_inside_sphere/n;
 pi_error = 6*std(r_inside)/sqrt(length(r));
 pi_lower = pi_approx - pi_error;
 pi_upper = pi_approx + pi_error;
-toc;
 
 fprintf('π_approx = %.5f\n', pi_approx);
 fprintf('error = %.5f\n', pi_error);
