90e1c02c9bfdf9b508bfd47fc25c24a81d32f6dc
retse <anast.rets@hotmail.com>
2021-04-11 21:20:57 +0300

    Create diagrams using a function and formatting changes

--- Courses/Computational Electromagnetism and Applications (ΥΦΥ203)/Problem Sets/set2_phase_velocity_vs_incidence_angle.py
+++ Courses/Computational Electromagnetism and Applications (ΥΦΥ203)/Problem Sets/set2_phase_velocity_vs_incidence_angle.py
@@ -7,23 +7,29 @@ def percent_stability_criterion(dx, dy, percent):
     # Input: dx, dy, percent
     # Output: dt
     if dx == dy:
-        dt = percent * dx/np.sqrt(2)
+        dt = percent * dx / np.sqrt(2)
     else:
-        dt = percent * 1/(np.sqrt((1/pow(dx, 2))+(1/pow(dy, 2))))
+        dt = percent * 1 / (np.sqrt((1 / pow(dx, 2)) + (1 / pow(dy, 2))))
     return dt
 
 
-def k_hat_newton(theta, dx, dy, dt, l):
+def k_hat_newton(theta, dx, dy, dt, lambd):
+    # Computes k_hat using the Newton-Raphson method
+    # Input: theta [deg], dx, dy, dt, l
+    # Output: k_hat
     theta = np.deg2rad(theta)
     # Define f and f dot
-    f = lambda khat: ((1/pow(dx, 2))*pow(np.sin(khat*np.cos(theta)*dx/2), 2)) + ((1/pow(dy, 2))*pow(np.sin(khat*np.sin(theta)*dy/2), 2)) - ((1/pow(dt, 2))*pow(np.sin(np.pi*dt/l),2))
-    fdot = lambda khat: (np.cos(theta)/(2*dx))*np.sin(khat*np.cos(theta)*dx) + (np.sin(theta)/(2*dy))*np.sin(khat*np.sin(theta)*dy)
+    f = lambda khat: ((1 / pow(dx, 2)) * pow(np.sin(khat * np.cos(theta) * dx / 2), 2)) + (
+                (1 / pow(dy, 2)) * pow(np.sin(khat * np.sin(theta) * dy / 2), 2)) - (
+                                 (1 / pow(dt, 2)) * pow(np.sin(np.pi * dt / lambd), 2))
+    fdot = lambda khat: (np.cos(theta) / (2 * dx)) * np.sin(khat * np.cos(theta) * dx) + (
+                np.sin(theta) / (2 * dy)) * np.sin(khat * np.sin(theta) * dy)
     # Stopping criteria
-    N = 5  # Number of significant digits to be computed
+    n = 10  # Number of significant digits to be computed
     max_repetitions = 1000
-    es = 0.5 * pow(10, (2 - N))  # Scarborough Criterion
+    es = 0.5 * pow(10, (2 - n))  # Scarborough Criterion
     ea = 100
-    k_prev = 2*np.pi/l
+    k_prev = 2 * np.pi / lambd
     repetitions = 0
     # Main Newton-Raphson loop
     while ea > es:
@@ -34,37 +40,48 @@ def k_hat_newton(theta, dx, dy, dt, l):
         if repetitions > max_repetitions:
             print('Max repetitions reached without achieving desired accuracy for E!')
             break
-    k = k_next
-    return k
+    k_hat = k_next
+    return k_hat
 
+
+def phase_velocity_vs_theta_diagram(dx, dy, dt, lambd, min_theta, max_theta):
+    # Creates a numerical phase velocity diagram vs theta for theta interval [min_theta, max_theta]
+    # Input: dx, dy, dt, lambd, min_theta, max_theta
+    # Output: None (can plot figure using plt.show())
+    step_size = 1
+    total_size = int(((max_theta - min_theta) / step_size) + 1)
+    vpc_values = np.zeros(total_size)
+    # Compute k and v_p/c values
+    for i in range(0, total_size):
+        theta = i * step_size
+        k = k_hat_newton(theta, dx, dy, dt, lambd)
+        vpc_values[i] = 2 * np.pi / (lambd * k)
+    # Plot output
+    plt.plot(vpc_values, label="$\Delta x = %.2f \lambda$\n$\Delta y = %.2f \lambda$" % (dx, dy))
+    plt.title("Numerical phase velocity vs θ (for Δt = 90% of stability limit)")
+    plt.ylabel(r"$v_{p}/c$")
+    plt.xlabel(r"$\theta$ [degrees]")
+    plt.legend()
+    return None
+
+
+theta_start = 0
+theta_end = 90
 
 # Case A: Delta_x = Delta_y = lambda/10, Delta_t = 0.9 C.L.
 lmbd = 1
-Delta_x = Delta_y = lmbd/10
+Delta_x = lmbd / 10
+Delta_y = lmbd / 10
 Delta_t = percent_stability_criterion(Delta_x, Delta_y, 0.9)
-
-vpc_values = np.zeros(90)
-
-for i in range(0, 90):
-    k = k_hat_newton(i, Delta_x, Delta_y, Delta_t, lmbd)
-    vpc_values[i] = 2*np.pi/(lmbd*k)
-
-plt.plot(vpc_values)
-plt.grid()
-plt.show()
+phase_velocity_vs_theta_diagram(Delta_x, Delta_y, Delta_t, lmbd, theta_start, theta_end)
 
 # Case B: Delta_x = lambda/10, Delta_y = lambda/20, Delta_t = 0.9 C.L.
 lmbd = 1
-Delta_x = lmbd/10
-Delta_y = lmbd/20
+Delta_x = lmbd / 10
+Delta_y = lmbd / 20
 Delta_t = percent_stability_criterion(Delta_x, Delta_y, 0.9)
+phase_velocity_vs_theta_diagram(Delta_x, Delta_y, Delta_t, lmbd, theta_start, theta_end)
 
-vpc_values = np.zeros(90)
-
-for i in range(0, 90):
-    k = k_hat_newton(i, Delta_x, Delta_y, Delta_t, lmbd)
-    vpc_values[i] = 2*np.pi/(lmbd*k)
-
-plt.plot(vpc_values)
+# Show resulting plot
 plt.grid()
 plt.show()
