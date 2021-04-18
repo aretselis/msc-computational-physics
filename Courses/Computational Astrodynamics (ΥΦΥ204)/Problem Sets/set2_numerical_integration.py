78f93973e695ee82fb668ec2ed09770eaaf37078
retse <anast.rets@hotmail.com>
2021-04-18 23:32:42 +0300

    Add constants and orbit definition

---
+++ Courses/Computational Astrodynamics (ΥΦΥ204)/Problem Sets/set2_numerical_integration.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Constants definition
+
+G = 6.6743*pow(10, -11)
+M_earth = 5.977*pow(10, 24)
+mu_earth = G*M_earth
+R_earth = 6371000  # [meters]
+
+# Orbit definition
+H = 400000  # [meters]
+a = R_earth + H
+i = 0
+e = 0
+Omega = 0
+omega = 0
+M = 0
+
