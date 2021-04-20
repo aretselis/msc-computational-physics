4ab0e4b0f0a2a0a947a310c538dfd4d62edc4179
retse <anast.rets@hotmail.com>
2021-04-20 23:14:27 +0300

    Fix an error in theta calculation

--- Courses/Computational Astrodynamics (ΥΦΥ204)/Problem Sets/set2_integrator_with_SRP.py
+++ Courses/Computational Astrodynamics (ΥΦΥ204)/Problem Sets/set2_integrator_with_SRP.py
@@ -14,7 +14,7 @@ def shadow_checker(x_sc, y_sc, z_sc, x_sun, y_sun, z_sun):
     r_spacecraft = np.sqrt(pow(x_sc, 2) + pow(y_sc, 2) + pow(z_sc, 2))
     r_sun = np.sqrt(pow(x_sun, 2) + pow(y_sun, 2) + pow(z_sun, 2))
     # Compute theta values
-    theta = np.arccos((x_sc*y_sc+y_sc*y_sun+z_sc*z_sun)/(r_spacecraft*r_sun))
+    theta = np.arccos((x_sc*x_sun+y_sc*y_sun+z_sc*z_sun)/(r_spacecraft*r_sun))
     theta_1 = np.arccos(r_earth/r_spacecraft)
     theta_2 = np.arccos(r_earth/r_sun)
     # Decide if we are inside or outside Earth's shadow
