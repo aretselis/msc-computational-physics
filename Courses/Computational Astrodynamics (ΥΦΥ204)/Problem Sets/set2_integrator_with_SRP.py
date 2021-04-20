8142cdf46c9af87ab2491aacfef456248151a750
retse <anast.rets@hotmail.com>
2021-04-20 22:54:19 +0300

    Implement function to check if spacecraft is inside the shadow

---
+++ Courses/Computational Astrodynamics (ΥΦΥ204)/Problem Sets/set2_integrator_with_SRP.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def shadow_checker(x_sc, y_sc, z_sc, x_sun, y_sun, z_sun):
+    # Computes if a spacecraft is inside Earth's shadow
+    # Input:
+    # Spacecraft position vector (x_sc, y_sc, z_sc) [meters]
+    # Sun position vector (x_sun, y_sun, z_sun) [meters]
+    # Output:
+    # 0, if the spacecraft is in Earth's shadow
+    # 1, if the spacecraft is in sunlight
+    r_earth = 6371000
+    # Compute vector magnitudes
+    r_spacecraft = np.sqrt(pow(x_sc, 2) + pow(y_sc, 2) + pow(z_sc, 2))
+    r_sun = np.sqrt(pow(x_sun, 2) + pow(y_sun, 2) + pow(z_sun, 2))
+    # Compute theta values
+    theta = np.arccos((x_sc*y_sc+y_sc*y_sun+z_sc*z_sun)/(r_spacecraft*r_sun))
+    theta_1 = np.arccos(r_earth/r_spacecraft)
+    theta_2 = np.arccos(r_earth/r_sun)
+    # Decide if we are inside or outside Earth's shadow
+    if theta_1+theta_2 <= theta:
+        result = 0
+    else:
+        result = 1
+    return result
