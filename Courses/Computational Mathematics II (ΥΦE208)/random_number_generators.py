298577619b520a35bd557439dad0c4c9b9e1f4d0
retse <anast.rets@hotmail.com>
2021-05-25 19:31:29 +0300

    Add x-y plot to see if a correlation exists

--- Courses/Computational Mathematics II (ΥΦE208)/random_number_generators.py
+++ Courses/Computational Mathematics II (ΥΦE208)/random_number_generators.py
@@ -14,9 +14,23 @@ def linear_congruential_generator(N):
     return numbers_list
 
 
-numbers = 100
+numbers = 1000
 result = linear_congruential_generator(numbers)
 plt.figure()
 plt.hist(result)
+plt.grid()
+
+x_vector = [0.5]
+y_vector = [0.0]
+for i in range(0, numbers-1):
+    if i == 0:
+        y_vector[0] = result[i]
+    elif i%2==0:
+        y_vector.append(result[i])
+    else:
+        x_vector.append(result[i])
+
+plt.figure()
+plt.scatter(x_vector, y_vector)
 plt.grid()
 plt.show()
