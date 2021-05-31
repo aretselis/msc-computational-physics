d886c388d039fc64117d42610fd1b4f22152d01e
retse <anast.rets@hotmail.com>
2021-05-31 17:00:43 +0300

    Revert "Upload OpenMP exam solutions"
    This reverts commit 1578efe1ff9abc6db37f5af3058dc40a44fd2229.

--- Courses/Computational Mathematics II (ΥΦE208)/random_number_generators.py
+++ Courses/Computational Mathematics II (ΥΦE208)/random_number_generators.py
@@ -13,32 +13,23 @@ def linear_congruential_generator(xn_prev, N):
     return numbers_list
 
 
-# numbers = 100000
-# result = linear_congruential_generator(0.02, numbers)
-# plt.figure()
-# plt.hist(result)
-# plt.grid()
-#
-# x_vector = [0.5]
-# y_vector = [0.0]
-# for i in range(0, numbers-1):
-#     if i == 0:
-#         y_vector[0] = result[i]
-#     elif i%2==0:
-#         y_vector.append(result[i])
-#     else:
-#         x_vector.append(result[i])
-#
-# plt.figure()
-# plt.scatter(x_vector, y_vector)
-# plt.grid()
-# plt.show()
-#
-# c = []
-# for i in range(1, 6):
-#     sum = 0
-#     for j in range(0, numbers-i):
-#         sum += result[j]*result[j+i]
-#     c.append(sum/j)
-#
-# print(c)
+numbers = 1000
+result = linear_congruential_generator(numbers)
+plt.figure()
+plt.hist(result)
+plt.grid()
+
+x_vector = [0.5]
+y_vector = [0.0]
+for i in range(0, numbers-1):
+    if i == 0:
+        y_vector[0] = result[i]
+    elif i%2==0:
+        y_vector.append(result[i])
+    else:
+        x_vector.append(result[i])
+
+plt.figure()
+plt.scatter(x_vector, y_vector)
+plt.grid()
+plt.show()
